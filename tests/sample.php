bb34525e588d01330a62c8c1c52393390fd40308
pokoot <pokoot@gmail.com>
2013-12-28 08:26:13 +0800

    remove spaces

--- tests/sample.php
+++ tests/sample.php
@@ -2,7 +2,6 @@
 
 $loader = require __DIR__ . "/../vendor/autoload.php";
 
-
 use Goldfinger\Config;
 
 Config::setFile("test.yml");
@@ -14,11 +13,3 @@ $config->username = "my_username";
 print "<Br> config 1 = " . $config->config_1;
 print "<Br> config 2 = " . $config->config_2;
 print "<Br> username = " . $config->username;
-
-
-
-
-
-
-
-
